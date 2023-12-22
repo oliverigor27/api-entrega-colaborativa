3117f28cea04988f6f26bbad052820c8f0ca567b
Igor Oliveira <igoroliveira9827@gmail.com>
2023-12-22 20:01:31 -0300

    refactor: remover UserRepository, IUserRepository e UserDTO

--- src/modules/user/repositories/IUser.repository.ts
+++
@@ -1,7 +0,0 @@
-import { UserDto } from "../dto/user.dto";
-
-export interface IUserInterface {
-    createUser(data: UserDto): Promise<UserDto>;
-    confirmPassWord(token: string): Promise<string>;
-    recoverPassword(email: string): boolean;
-}
