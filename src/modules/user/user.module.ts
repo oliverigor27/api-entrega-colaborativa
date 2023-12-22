56e090b3074ad3622a50e7a9d87555e3eab54d32
Igor Oliveira <igoroliveira9827@gmail.com>
2023-12-22 20:02:06 -0300

    refactor: refactor all user archives in the project

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
-import { UserRepository } from "./repositories/User.repository";
-import { UserService } from "./services/User.service";
-import { UserController } from "./controllers/User.controller";
 
 @Module({
-    controllers: [UserController],
-    providers: [UserRepository, UserService],
+    controllers: [],
+    providers: [],
     imports: []
 })
 export class UserModule {}
