e479e38285fc0baae4af354d7b98e0c6dd29fe01
Igor Oliveira <igoroliveira9827@gmail.com>
2024-01-05 17:47:54 -0300

    feat: update user module by import new dependencies and set a new controller

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { UserConfigService } from "./services/userConfig.service";
+import { UserConfigController } from "./controllers/userConfig.controller";
+import { UserConfigRepository } from "./repository/UserConfig/userConfig.repository";
+import { PrismaModule } from "src/infra/prisma/prisma.module";
 
 @Module({
-    controllers: [],
-    providers: [],
-    imports: []
+    controllers: [UserConfigController],
+    providers: [UserConfigService, UserConfigRepository],
+    imports: [PrismaModule]
 })
 export class UserModule {}
