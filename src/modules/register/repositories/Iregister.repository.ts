dadc5b2e2ad4154528e9edd50813417a4e0a8925
Igor Oliveira <igoroliveira9827@gmail.com>
2024-02-23 01:01:30 -0300

    refactor: update promises return

--- src/modules/register/repositories/Iregister.repository.ts
+++ src/modules/register/repositories/Iregister.repository.ts
@@ -1,7 +1,7 @@
 import { CompleteRegisterDTO } from "../../users/dto/completeRegister.dto";
-import { RegisterDto } from "../dto/register.dto";
+import { RegisterDto } from "../Dto/register.dto";
 
 export interface IRegisterInterface {
-    createUser(data: RegisterDto): Promise<{ message: string, id: string }>;
-    createPostman(data: RegisterDto): Promise<{ message: string, id: string }>;
+    createUser(data: RegisterDto): Promise<{ isValid: boolean, id: string } | boolean>;
+    createPostman(data: RegisterDto): Promise<{ message: string, id: string } | boolean>;
 }
