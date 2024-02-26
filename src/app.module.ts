4c29c95c254e2dfbca67051b6195e95e819dfa92
Igor Oliveira <igoroliveira9827@gmail.com>
2024-02-26 00:36:38 -0300

    feat: config for use env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './modules/users/user.module';
 import { RegisterModule } from './modules/register/register.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [UserModule, RegisterModule],
+  imports: [ConfigModule.forRoot(), UserModule, RegisterModule],
   controllers: [],
   providers: [AppService],
 })
