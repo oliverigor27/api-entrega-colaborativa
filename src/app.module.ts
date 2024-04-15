773c166e035ea65c40a804ba9149a4593dc1669a
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-15 19:49:50 -0300

    feat: add auth module in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/users/user.module';
 import { RegisterModule } from './modules/register/register.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, RegisterModule],
+  imports: [ConfigModule.forRoot(), UserModule, RegisterModule, AuthModule],
   controllers: [],
   providers: [AppService],
 })
