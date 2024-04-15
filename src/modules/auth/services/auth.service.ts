2b994c0272fe1faca6c72f6f723bd9899acc29a8
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-15 19:50:11 -0300

    feat: add new create token in auth service

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { LoginDto } from "src/modules/auth/Dto/login.dto";
+
+
 
 @Injectable()
-export class AuthService 
+export class AuthService
 {
-    constructor(private readonly jwtService : JwtService) {}
-
-    async createAcessToken()
+    constructor(private readonly jwtService: JwtService)
     {
-        // return this.jwtService.sign()
+    }
+
+    generateToken(data: LoginDto)
+    {
+        return this.jwtService.sign({
+           sub: data.email,
+        },
+        {
+            expiresIn: "7 days",
+            issuer: "entrega",
+        }
+    )
+    }
+
+    singIn(data: LoginDto)
+    {
+        let token = this.generateToken(data);
+
+        return token;
     }
 }
