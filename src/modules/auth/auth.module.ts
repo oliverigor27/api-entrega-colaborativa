95ba3085c653775af036d5e12a584c2120b37e9c
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-15 19:50:33 -0300

    feat: add controller and auth service in the auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./controllers/auth.controller";
+import { AuthService } from "./services/auth.service";
 
 @Module({
     imports: [ JwtModule.register({
         secret: process.env.JWT_SECRET
-    }) ]
+    }) ],
+    controllers: [AuthController],
+    providers: [AuthService]
 })
 export class AuthModule {};
