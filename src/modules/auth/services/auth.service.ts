c662de910fab834a096662c75ef790bda88829bb
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-16 18:49:31 -0300

    refactor: refactor the response type for auth service

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -4,44 +4,26 @@ import { LoginDto } from "src/modules/auth/Dto/login.dto";
 import * as bcrypt from "bcrypt";
 import { PrismaService } from "src/infra/prisma/services/prisma.service";
 
-
 @Injectable()
 export class AuthService
 {
     constructor(
         private readonly jwtService: JwtService,
         private readonly prismaService : PrismaService
-    )
-    {}
+    ) {}
 
-    generateToken(email: string)
+    async validateUser(email: string, password: string)
     {
-        return this.jwtService.sign({
-           sub: email,
-        },
-        {
-            expiresIn: "7 days",
-            issuer: "entrega",
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email
         }
-    )
-    }
+      });
 
-
-    async singIn({ email, password }: LoginDto) {
-          const user = await this.prismaService.user.findUnique({
-            where: {
-              email,
-            },
-          });
-    
-          if (!user) throw new BadRequestException("User cannot be found!");
-    
-          const isPasswordValid = await bcrypt.compare(password, user.password);
-    
-          if (!isPasswordValid) throw new BadRequestException("Password invalid!");
-    
-          const token = this.generateToken(email);
-    
-          return token;
+      if (user && (await bcrypt.compare(password, user.password))) 
+      {
+        return this.jwtService.sign({ id_card: user.id_card });
       }
+      return null;
+    }
 }
