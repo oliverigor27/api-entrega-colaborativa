8921999d8cd57b0d0bfc3321dc9faf8daa12a5d0
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-16 18:49:58 -0300

    refactor: refactpr the req type for controller

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,8 +1,9 @@
-import { BadRequestException, Body, Controller, Post, Res } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post, Req, Res, UnauthorizedException, UseGuards } from "@nestjs/common";
 import { LoginDto } from "src/modules/auth/Dto/login.dto";
 import { AuthService } from "../services/auth.service";
-import { Response } from "express";
+import { Request, Response } from "express";
 import { ApiTags } from "@nestjs/swagger";
+import { LocalAuthGuard } from "../guards/local-auth.guard";
 
 @ApiTags("User Auth")
 @Controller("Auth")
@@ -11,9 +12,14 @@ export class AuthController
     constructor(private readonly authService: AuthService) {}
 
     @Post("sign-in")
-    async loginUSer(@Body() { email, password }: LoginDto, @Res() res: Response)
+    @UseGuards(LocalAuthGuard)
+    async loginUSer(@Req() req: Request, @Res() res: Response)
     {
-        let authToken = this.authService.singIn({ email, password });
+        const { email, password } = req.body;
+
+        let authToken = await this.authService.validateUser(email, password);
+
+        if(authToken == null) throw new UnauthorizedException("Cannot return!");
 
         return res.status(200).json({
             message: "Successful login!",
